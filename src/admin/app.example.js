52ae9fb1f537d15d4631d35cac42cfbfb65be634
divyansh360circle <divyansh@360circleinfotech.com>
2023-07-28 13:18:30 +0530

    first

---
+++ src/admin/app.example.js
@@ -0,0 +1,42 @@
+const config = {
+  locales: [
+    // 'ar',
+    // 'fr',
+    // 'cs',
+    // 'de',
+    // 'dk',
+    // 'es',
+    // 'he',
+    // 'id',
+    // 'it',
+    // 'ja',
+    // 'ko',
+    // 'ms',
+    // 'nl',
+    // 'no',
+    // 'pl',
+    // 'pt-BR',
+    // 'pt',
+    // 'ru',
+    // 'sk',
+    // 'sv',
+    // 'th',
+    // 'tr',
+    // 'uk',
+    // 'vi',
+    // 'zh-Hans',
+    // 'zh',
+  ],
+  auth: {
+    logo: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
+  },
+};
+
+const bootstrap = (app) => {
+  console.log(app);
+};
+
+export default {
+  config,
+  bootstrap,
+};
