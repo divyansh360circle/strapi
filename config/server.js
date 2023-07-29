5d298e3d4974d50b73660b8fc2a55f47dc05c555
divyansh360circle <divyansh@360circleinfotech.com>
2023-07-29 13:46:19 +0530

    server js changes

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  // port: env.int('PORT', 1337),
+  port: process.env.PORT || 1337,
   url: 'http://strapi.qafto.in',
   app: {
     keys: env.array('APP_KEYS'),
